23ff2b2d4538dafb59e91100c3993b5fd74e3cb3
Volodymyr75 <strembov@gmail.com>
2018-01-26 11:30:49 +0200

    del swiping

--- three/js/common.js
+++ three/js/common.js
@@ -140,21 +140,11 @@ var swiper = new Swipe('.s-reviews__item');
 // console.log(swiper.element.className);
 
 swiper.onLeft(function() {
- slides[currentSlide].className = 's-reviews__item';
-  currentSlide = (currentSlide+1)%slides.length;
-  slides[currentSlide].className = 's-reviews__item s-reviews__item--active'; });
+ });
 swiper.run();
 
 swiper.onRight(function() {
-  if(currentSlide == 0) {
-    slides[currentSlide].className = 's-reviews__item';
-    currentSlide = slides.length - 1;
-    slides[currentSlide].className = 's-reviews__item s-reviews__item--active';
-  }else {
-    slides[currentSlide].className = 's-reviews__item';
-    currentSlide = currentSlide - 1;
-    slides[currentSlide].className = 's-reviews__item s-reviews__item--active';
-  } });
+  });
 swiper.run();
 
 function initialize() {
