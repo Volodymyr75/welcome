1ed59ff839063a82e75e2c470efe9eb4f904dd5a
Volodymyr75 <strembov@gmail.com>
2018-08-15 19:32:52 +0300

    add all files

--- eight/js/common.js
+++ eight/js/common.js
@@ -7,27 +7,31 @@ $(document).ready(function() {
 
 	$(".btn--send, .social__link--mail").on("click", function(event) {
 		  event.preventDefault();
-		  $(".modal-mail").addClass("modal-mail--show");
+		  $(".modal-wrapper-mail").addClass("modal-wrapper-mail--show");
 		  $(".modal-overlay").addClass("modal-overlay--show");
 	});
 	
 	$(".btn--call, .social__link--phone").on("click", function(event) {
 		  event.preventDefault();
-		  $(".modal-call").addClass("modal-call--show");
+		  $(".modal-wrapper-call").addClass("modal-wrapper-call--show");
 		  $(".modal-overlay").addClass("modal-overlay--show");
 	});
 
 	$(".modal-mail__close, .modal-overlay").on("click", function(event) {
 		  event.preventDefault();
-		  $(".modal-mail--show").removeClass("modal-mail--show");
+		  $(".modal-wrapper-mail--show").removeClass("modal-wrapper-mail--show");
 		  $(".modal-overlay--show").removeClass("modal-overlay--show");
 	});
 
 	$(".modal-call__close, .modal-overlay").on("click", function(event) {
 		  event.preventDefault();
-		  $(".modal-call--show").removeClass("modal-call--show");
+		  $(".modal-wrapper-call--show").removeClass("modal-wrapper-call--show");
 		  $(".modal-overlay--show").removeClass("modal-overlay--show");
 	});
+
+
+
+	
 
 
     $(".s-order__up").bind('click', function(e){
