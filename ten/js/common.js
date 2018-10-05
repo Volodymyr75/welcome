dd1a188c2319380e453d3e4f34922e9a484ec525
Volodymyr75 <strembov@gmail.com>
2018-10-05 16:37:55 +0300

    change mobile menu directory ten

--- ten/js/common.js
+++ ten/js/common.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+
+
+	var $hamburger = $(".hamburger");
+			  $hamburger.on("click", function(e) {
+			  $(this).toggleClass("is-active");
+			  $(".page-header__group-menu").toggleClass("active");
+	    // Do something else, like open/close menu
+	  });
+
 	// var $navMain = $(".main-nav");
 	// $navMain.removeClass("main-nav--nojs");
 
@@ -12,10 +21,10 @@ $(document).ready(function() {
  //  });
 
 
-	$(".main-nav__toggle").click(function() {
-		$(".main-nav__sandwich").toggleClass("active");
-		$(".main-nav__items").toggleClass("main-nav__items--no");
-	});
+	// $(".main-nav__toggle").click(function() {
+	// 	$(".main-nav__sandwich").toggleClass("active");
+	// 	$(".main-nav__items").toggleClass("main-nav__items--no");
+	// });
 
 	$(".btn--get").on("click", function(event) {
 		  event.preventDefault();
@@ -23,9 +32,15 @@ $(document).ready(function() {
 		  $(".modal-overlay").addClass("modal-overlay--show");
 	});
 	
-	$(".btn--form-order, .btn--ask").on("click", function(event) {
+	$(".btn--form-order, .btn--ask, .s-inform__link").on("click", function(event) {
 		  event.preventDefault();
 		  $(".modal-wrapper-call").addClass("modal-wrapper-call--show");
+		  $(".modal-overlay").addClass("modal-overlay--show");
+	});
+
+	$(".btn--excur").on("click", function(event) {
+		  event.preventDefault();
+		  $(".modal-wrapper-excur").addClass("modal-wrapper-excur--show");
 		  $(".modal-overlay").addClass("modal-overlay--show");
 	});
 
@@ -35,9 +50,17 @@ $(document).ready(function() {
 		  $(".modal-overlay--show").removeClass("modal-overlay--show");
 	});
 
+
+
 	$(".modal-call__close, .modal-overlay").on("click", function(event) {
 		  event.preventDefault();
 		  $(".modal-wrapper-call--show").removeClass("modal-wrapper-call--show");
+		  $(".modal-overlay--show").removeClass("modal-overlay--show");
+	});
+
+	$(".modal-excur__close, .modal-overlay").on("click", function(event) {
+		  event.preventDefault();
+		  $(".modal-wrapper-excur--show").removeClass("modal-wrapper-excur--show");
 		  $(".modal-overlay--show").removeClass("modal-overlay--show");
 	});
 
@@ -51,7 +74,7 @@ $(document).ready(function() {
         $('body,html').animate({scrollTop: 0}, 800);    
     });
 
-    $(".social__link--phone, .social__link--mail, .page-header__item").bind('click', function(e){
+    $(".social__link--phone, .social__link--mail").bind('click', function(e){
         e.preventDefault();
         $('html, body').animate({
 	        scrollTop: $("#contacts").offset().top
@@ -59,6 +82,24 @@ $(document).ready(function() {
     });
 
 });
+
+
+$(".menu-span").on('click', function(e) { 
+    e.preventDefault();
+
+    var menu = $(this).next();
+    var isClosed = menu.is(':hidden'); // закрыто ли подменю, по которому кликнули 
+ 
+    $('.main-nav__submenu').slideUp(100); // закрываем все подменю
+    $(".menu-span").removeClass("active");
+ 
+    // если меню было закрыто, то открываем его 
+    if (isClosed) { 
+      menu.slideDown(200);
+      $(this).addClass("active");
+    } 
+  }); 
+
 
 
 // $(window).load(function() {
